7db475845a446893596574ee3151e0ed8417b389
anas <anasmj7@gmail.com>
2024-05-22 13:53:53 +0600

    updated dependency code

--- lib/printer.dart
+++ lib/printer.dart
@@ -1,11 +1,12 @@
-import 'dart:typed_data';
-
 import 'package:flutter/services.dart';
 
-final flutterPrinterChannel = const MethodChannel('flutter_pos_printer_platform');
-final flutterPrinterEventChannel = const EventChannel('flutter_pos_printer_platform_stream');
+final flutterPrinterChannel =
+    const MethodChannel('flutter_pos_printer_platform');
+final flutterPrinterEventChannel =
+    const EventChannel('flutter_pos_printer_platform_stream');
 final iosChannel = const MethodChannel('flutter_pos_printer_platform/methods');
-final iosStateChannel = const EventChannel('flutter_pos_printer_platform/state');
+final iosStateChannel =
+    const EventChannel('flutter_pos_printer_platform/state');
 
 enum BTStatus { none, connecting, connected, scanning, stopScanning }
 
